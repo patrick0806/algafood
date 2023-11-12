22c10600cd305ef401ba87e58b16ca3980c9f2bd
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-12 19:42:22 -0300

    feat: add others crud operations on kitchen category controller

--- src/main/java/com/nicezi/patrick/algafood/api/controller/KitchenCategoryController.java
+++ src/main/java/com/nicezi/patrick/algafood/api/controller/KitchenCategoryController.java
@@ -3,11 +3,15 @@ package com.nicezi.patrick.algafood.api.controller;
 
 import com.nicezi.patrick.algafood.domain.model.KitchenCategory;
 import com.nicezi.patrick.algafood.domain.repository.KitchenCategoryRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +50,38 @@ public class KitchenCategoryController {
         final var savedKitchenCategory = this.kitchenCategoryRepository.save(kitchenCategory);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(savedKitchenCategory);
+    }
+
+    @PutMapping("/{kitchenCategoryId}")
+    public ResponseEntity<KitchenCategory> updateKitchenCategory(
+            @PathVariable Long kitchenCategoryId,
+            @RequestBody  KitchenCategory kitchenCategory){
+        final var currentKitchenCategory = this.kitchenCategoryRepository.findById(kitchenCategoryId);
+
+        if(currentKitchenCategory == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        BeanUtils.copyProperties(kitchenCategory, currentKitchenCategory,"id");
+
+        final var savedKitchenCategory = this.kitchenCategoryRepository.save(currentKitchenCategory);
+
+        return ResponseEntity.ok(savedKitchenCategory);
+    }
+
+    @DeleteMapping("/{kitchenCategoryId}")
+    public ResponseEntity<KitchenCategory> deleteKitchenCategory(@PathVariable Long kitchenCategoryId){
+        try{
+            KitchenCategory currentKitchenCategory = this.kitchenCategoryRepository.findById(kitchenCategoryId);
+
+            if(currentKitchenCategory == null){
+                return ResponseEntity.notFound().build();
+            }
+
+            this.kitchenCategoryRepository.remove(kitchenCategoryId);
+            return ResponseEntity.noContent().build();
+        } catch (DataIntegrityViolationException ex){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }

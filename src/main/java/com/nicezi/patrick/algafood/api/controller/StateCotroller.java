5d4ed949fce4112c8ff1ea5d6ab1a54e50da5ecf
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-12 18:51:06 -0300

    refactor: using produces to return only json

--- src/main/java/com/nicezi/patrick/algafood/api/controller/StateCotroller.java
+++ src/main/java/com/nicezi/patrick/algafood/api/controller/StateCotroller.java
@@ -2,6 +2,7 @@ package com.nicezi.patrick.algafood.api.controller;
 
 import com.nicezi.patrick.algafood.domain.model.State;
 import com.nicezi.patrick.algafood.domain.repository.StateRepository;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,10 @@ public class StateCotroller {
         this.stateRepository = stateRepository;
     }
 
-    @GetMapping
+    // the procues is using when you can return more than one type of resposne
+    // like json and xml but with procudes you force return only json
+    // this property can stay in top of class
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<State> list(){
         return this.stateRepository.list();
     }

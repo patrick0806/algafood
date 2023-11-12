9fb9cf4c78ff2f56bb47a709c4868852e7b7eb68
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-12 14:00:40 -0300

    feat: add autoincrement id

--- src/main/java/com/nicezi/patrick/algafood/domain/model/KitchenCategory.java
+++ src/main/java/com/nicezi/patrick/algafood/domain/model/KitchenCategory.java
@@ -2,6 +2,8 @@ package com.nicezi.patrick.algafood.domain.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,6 +11,7 @@ import jakarta.persistence.Table;
 @Table(name = "kitchen_categories")
 public class KitchenCategory {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
--- src/main/java/com/nicezi/patrick/algafood/domain/model/Restaurant.java
+++ src/main/java/com/nicezi/patrick/algafood/domain/model/Restaurant.java
@@ -2,6 +2,8 @@ package com.nicezi.patrick.algafood.domain.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,6 +13,7 @@ import java.math.BigDecimal;
 @Table(name = "restaurants")
 public class Restaurant {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 

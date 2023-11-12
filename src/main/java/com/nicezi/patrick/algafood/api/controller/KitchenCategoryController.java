9f5609ffd49674414b44760f204b639b46d4813d
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-12 19:23:20 -0300

    feat: create route to create a mew kitchen category

--- src/main/java/com/nicezi/patrick/algafood/api/controller/KitchenCategoryController.java
+++ src/main/java/com/nicezi/patrick/algafood/api/controller/KitchenCategoryController.java
@@ -3,9 +3,12 @@ package com.nicezi.patrick.algafood.api.controller;
 
 import com.nicezi.patrick.algafood.domain.model.KitchenCategory;
 import com.nicezi.patrick.algafood.domain.repository.KitchenCategoryRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +24,10 @@ public class KitchenCategoryController {
     }
 
     @GetMapping
-    public List<KitchenCategory> list(){
-        return this.kitchenCategoryRepository.list();
+    public ResponseEntity<List<KitchenCategory>> list(){
+        final var kitchens = this.kitchenCategoryRepository.list();
+
+        return ResponseEntity.ok(kitchens);
     }
 
     @GetMapping("/{kitchenCategoryId}")
@@ -34,5 +39,12 @@ public class KitchenCategoryController {
         };
 
         return ResponseEntity.ok(kitchenCategory);
+    }
+
+    @PostMapping
+    public ResponseEntity<KitchenCategory> createKitchenCategory(@RequestBody  KitchenCategory kitchenCategory){
+        final var savedKitchenCategory = this.kitchenCategoryRepository.save(kitchenCategory);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedKitchenCategory);
     }
 }

afc84fd76bbed30cd061d68d1f4b184d34e6a372
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-12 12:29:15 -0300

    chore: init algafood spring project

---
+++ src/test/java/com/nicezi/patrick/algafood/AlgafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicezi.patrick.algafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

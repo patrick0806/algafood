4ee7f42ea00ffa40ca1262d41d0b2b1e8f1891a6
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-14 23:40:47 -0300

    refactor: stop using @component and start using @Repository

--- src/main/java/com/nicezi/patrick/algafood/infra/repository/RestaurantRepositoryImpl.java
+++ src/main/java/com/nicezi/patrick/algafood/infra/repository/RestaurantRepositoryImpl.java
@@ -9,11 +9,12 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Component
+@Repository
 public class RestaurantRepositoryImpl implements RestaurantRepository {
     @PersistenceContext
     private EntityManager manager;

18a558123f033cc1986349a5d10ac33a3ed20b28
Patrick Nicezi <patrickk0806@gmail.com>
2023-11-17 17:19:21 -0300

    test: create first integration test

--- src/test/java/com/nicezi/patrick/algafood/AlgafoodApplicationTests.java
+++ src/test/java/com/nicezi/patrick/algafood/AlgafoodApplicationTests.java
@@ -1,13 +1,33 @@
 package com.nicezi.patrick.algafood;
 
+import static org.junit.jupiter.api.Assertions.*;
+import com.nicezi.patrick.algafood.domain.model.GastronomyStyle;
+import com.nicezi.patrick.algafood.domain.service.GastronomyStyleService;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class AlgafoodApplicationTests {
 
+	@Autowired
+	private GastronomyStyleService gastronomyStyleService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void createGastronomyStyleWithSuccess(){
+		final var gastronomyStyle = new GastronomyStyle();
+		gastronomyStyle.setName("Chinesa");
+
+		final var newGastronomyStyle = gastronomyStyleService.save(gastronomyStyle);
+
+		assertNotNull(newGastronomyStyle);
+		assertNotNull(newGastronomyStyle.getId());
 	}
 
 }
